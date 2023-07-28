bbbe13e18f8ab5e46b47cb5d4ded43c9fc62e7ac
TwiDev <twidev5@gmail.com>
2023-07-28 15:51:24 +0200

    add header to responses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use database::UserData;
 use rocket::{get, http::Status, serde::json::Json};
 use serde::{Serialize};
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
 
 
 #[macro_use]
@@ -44,11 +47,30 @@ fn not_found() -> Json<GenericResponse> {
     })
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     database::initialize();
 
-    rocket::build().mount("/", routes![get_user_handler]).register("/", catchers![not_found])
+    rocket::build().mount("/", routes![get_user_handler]).register("/", catchers![not_found]).attach(CORS)
 }
 
 
