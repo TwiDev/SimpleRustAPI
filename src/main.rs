c58db7fcf2d774da9d7633195cbf290ff03857c1
TwiDev <twidev5@gmail.com>
2023-07-28 16:36:44 +0200

    add post create user action

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use database::UserData;
 use rocket::{get, http::Status, serde::json::Json};
 use serde::{Serialize};
 use rocket::http::Header;
-use rocket::{Request, Response};
+use rocket::{Request, Response, response::status::Custom};
 use rocket::fairing::{Fairing, Info, Kind};
 
 
@@ -23,10 +23,20 @@ pub struct UserResponse {
     pub message: UserData
 }
 
+
+#[derive(Serialize, Debug)]
+pub struct PostUserResponse {
+    pub status: String,
+    pub message: String,
+    pub user_id: u64
+}
+
 #[get("/users/<id>")]
 async fn get_user_handler(id: i32) -> Result<Json<UserResponse>, Status> {
-    if id == 1 {
-        let user:UserData = database::get_user(id).await;
+    let optional_user:Option<UserData> = database::get_user(id).await;
+
+    if optional_user.is_some() {
+        let user: UserData = optional_user.unwrap();
 
         Ok(Json(UserResponse {
             status: "success".to_string(),
@@ -37,6 +47,18 @@ async fn get_user_handler(id: i32) -> Result<Json<UserResponse>, Status> {
     } else {
         Err(Status::NotFound)
     }
+}
+
+#[post("/users", data="<body>")]
+async fn post_user_handler(mut body: String) -> Result<Json<PostUserResponse>, Custom<Json<GenericResponse>>> {
+    println!("{:?}", body);
+    let user:u64 = database::create_user(body, 0).await.expect("Error creating user");
+
+    Ok(Json(PostUserResponse{
+        status: "success".to_string(),
+        message: "User created".to_string(),
+        user_id: user
+    }))
 }
 
 #[catch(404)] 
@@ -70,14 +92,14 @@ impl Fairing for CORS {
 fn rocket() -> _ {
     database::initialize();
 
-    rocket::build().mount("/", routes![get_user_handler]).register("/", catchers![not_found]).attach(CORS)
+    rocket::build().mount("/", routes![get_user_handler, post_user_handler]).register("/", catchers![not_found]).attach(CORS)
 }
 
 
 pub mod database {
     use mysql::{*, prelude::Queryable};
     use once_cell::sync::OnceCell;
-    use serde::{Serialize};
+    use serde::{Serialize,Deserialize};
 
     const URL: &str = "mysql://root:root@localhost:3306/core";
     static DB_POOL: OnceCell<Pool> = OnceCell::new();
@@ -103,21 +125,36 @@ pub mod database {
         DB_POOL.set(Pool::new(URL).unwrap()).unwrap();
     }
 
-    #[derive(Serialize, Debug, PartialEq, Eq)]
+    #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
     pub struct UserData {
         pub id: i32,
         pub name: String,
         pub power: i32,
     }
 
-    pub async fn get_user(id: i32) -> UserData{
+    pub async fn get_user(id: i32) -> Option<UserData> {
         let mut _conn: PooledConn = DATABASE_CLIENT.database_conn().await;
 
         let _query: String = format!("SELECT * FROM users WHERE id = {}", id);
         
         _conn.query_map(_query, |(id, name, power)| {
             UserData { id, name, power }
-        }).unwrap().pop().unwrap()
+        }).unwrap().pop()
+    }
+
+    pub async fn create_user(name: String,power:i32) -> std::result::Result<u64, mysql::error::Error>{
+        let mut _conn: PooledConn = DATABASE_CLIENT.database_conn().await;
+
+        let _query: String = format!("INSERT INTO users (name, power) VALUES (:name, :power)");
+        
+        _conn.exec_drop(_query, 
+            params! {
+            "name" => name, 
+            "power" => power,
+            },
+        ).and_then(|_| 
+            Ok(_conn.last_insert_id()))
+
     }
 
 
